60635d06199ba0a5f1da6fa6cca85fcfdab6bf9d
--add <vlad.onischuk1234@gmail.com>
2022-02-17 11:20:19 +0200

    First Flask Project done

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from .views.cars import cars_bp
 
 
+def setup_swagger(app):
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.yaml'
+    swagger_bp = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "Lecture12_Example1"
+        }
+    )
+    app.register_blueprint(swagger_bp, url_prefix=SWAGGER_URL)
+
+
 def create_app():
     app = Flask(__name__)
-    # app.config["SERVER_NAME"] = "127.0.0.1:5000"
-    app.config['FLASK_DEBUG'] = 1
+    setup_swagger(app)
     app.register_blueprint(cars_bp)
     return app
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from app.app import create_app
 DEBUG = True
 app = create_app()
 
+
 if __name__ == "__main__":
     app.config.from_object(__name__)
     app.run(debug=True)
